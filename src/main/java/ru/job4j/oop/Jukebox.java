524f14d8ea7a336b93b4d115f8b88a5bf8995557
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-12-12 19:50:49 +0500

    1.3. Вызов метода с аргументами. [#166086]

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+import java.util.jar.JarEntry;
+
+public class Jukebox {
+    public void music(int position) {
+        if (position == 1) {
+            System.out.println("Пусть бешут неуклюже");
+        } else if (position == 2) {
+                System.out.println("Спокойной ночи");
+        } else if (position != 1 && position != 2) {
+                System.out.println("Песня не найдена");
+            }
+        }
+
+    public static void main(String[] args) {
+        Jukebox range = new Jukebox();
+        range.music(1);
+        range.music(2);
+        range.music(3);
+    }
+}
