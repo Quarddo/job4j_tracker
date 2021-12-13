5afe48071590f5c40852b69306090326d3e596a2
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-12-13 15:02:13 +0500

    1.4. Вызов метода с возвращаемым типом. [#167387 #239386]

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -1,17 +1,8 @@
 package ru.job4j.oop;
 
 public class DummyDic {
-    public String english() {
-        String word = "Dance";
-        return word;
-    }
 
     public String engToRus(String eng) {
-        return ("Неизвестное слово " + eng);
-    }
-
-    public String engTo(String eng) {
-        eng = "Cop";
-        return (" Неизвестное слово " + eng);
+        return "Неизвестное слово " + eng;
     }
 }
