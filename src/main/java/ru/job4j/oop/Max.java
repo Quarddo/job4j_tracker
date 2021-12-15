3f606bf645474ee6697debae56b881af438775e4
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-12-15 22:49:09 +0500

    3. Перегрузить метод max для трех чисел [#122644 #239935]

--- src/main/java/ru/job4j/oop/Max.java
+++ src/main/java/ru/job4j/oop/Max.java
@@ -10,18 +10,22 @@ public class Max {
     public static int max(int left, int right, int up) {
         boolean condition = left >= right;
         int result = condition ? (left > up ? left : up) : (right > up ? right : up);
-        return result;
+        return max(max(left, right), up);
     }
 
     public static int max(int left, int right, int up, int down) {
         int maxi = left > right ? left : right;
         maxi = maxi > up ? maxi : up;
         maxi = maxi > down ? maxi : down;
-        return maxi;
+        return max(max(left, right, up), down);
     }
 
     public static void main(String[] args) {
         int sq = Max.max(3, 2);
         System.out.println(sq);
+        int maxNumb = Max.max(1, 4, 2);
+        System.out.println(maxNumb);
+        int maxNumber = Max.max(10, 5, 15, 25);
+        System.out.println(maxNumber);
     }
 }
