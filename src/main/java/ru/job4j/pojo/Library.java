a80199de5a6cf88be563b323df145b5932881b25
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-12-18 19:10:08 +0500

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,30 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book moon = new Book("Moon", 100);
+        Book sun = new Book("Sun", 110);
+        Book wind = new Book("Wind", 90);
+        Book cleanCode = new Book("Clean code", 85);
+        Book[] books = {moon, sun, wind, cleanCode};
+        for (int i = 0; i < books.length; i++) {
+            Book prt = books[i];
+            System.out.println("Книга " + prt.getName() + " имеет " + prt.getCountPage() + " страниц.");
+        }
+        System.out.println(System.lineSeparator() + "Поменяем местами книги \"Moon\" и \"Clean code\".");
+        books[0] = cleanCode;
+        books[3] = moon;
+        for (int i = 0; i < books.length; i++) {
+            Book prt = books[i];
+            System.out.println("Книга " + prt.getName() + " имеет " + prt.getCountPage() + " страниц.");
+        }
+        System.out.println(System.lineSeparator() + "Вывод книг с именем \"Clean code\".");
+        for (int i = 0; i < books.length; i++) {
+            Book prt = books[i];
+            if (prt.getName().equals("Clean code")) {
+             System.out.println("Книга " + prt.getName() + " имеет " + prt.getCountPage() + " страниц.");
+            }
+        }
+    }
+}
