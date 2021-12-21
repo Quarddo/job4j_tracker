890a21989da98ff84dd3874e55a2031f112ed55a
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-12-21 20:55:13 +0500

    9. toString [#310018 #242563]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,8 +6,6 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        LocalDateTime created = item.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(created.format(formatter));
+        System.out.println(item.toString());
     }
 }
