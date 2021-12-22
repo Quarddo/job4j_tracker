de9dc6ea9dee5d30c2b4737e988b4bd99170c16d
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-12-22 14:04:09 +0500

    7. Метод удаления заявки Tracker.delete [#211749 #242540]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -67,10 +67,8 @@ public class Tracker {
     public boolean delete(int id) {
         int index = indexOf(id);
         if (index != -1) {
-            int start = index + 1;
-            Item rst = items[index];
-            int distPos = index;
-            int length = size - index - 1;
+            System.arraycopy(items, index + 1,
+                    items, index, size - index - 1);
             items[size - 1] = null;
             size--;
             return true;
