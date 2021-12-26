98e7395ef0e76c3385f55af0de683493ab0b4eb8
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-12-26 16:06:56 +0500

    7. Шаблон проектирования - Стратегия [#786 #244246]

--- src/main/java/ru/job4j/strategy/Paint.java
+++ src/main/java/ru/job4j/strategy/Paint.java
@@ -2,6 +2,7 @@ package ru.job4j.strategy;
 
 public class Paint {
     public void draw(Shape shape) {
+        System.out.print(shape.draw());
     }
 
     public static void main(String[] args) {
--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -5,12 +5,8 @@ public class Square implements Shape {
     @Override
     public String draw() {
         String ln = System.lineSeparator();
-        return    " _____" + ln
-                + "|     |" + ln
-                + "|_____|" + ln;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Square().draw());
+        return    "  _____" + ln
+                + " |     |" + ln
+                + " |_____|" + ln;
     }
 }
