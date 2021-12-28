5435e5286249940353f663ea08c55228a35c9bc2
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-12-28 18:55:45 +0500

    9.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/StubOutput.java
+++ src/main/java/ru/job4j/tracker/StubOutput.java
@@ -3,7 +3,6 @@ package ru.job4j.tracker;
 public class StubOutput implements Output {
     private final StringBuilder buffer = new StringBuilder();
 
-
     @Override
     public void println(Object obj) {
         if (obj != null) {
