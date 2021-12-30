f97a2ec2a6fbb3617a4eb2aecd2f08435392d883
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-12-30 13:34:24 +0500

    0.5. Иерархия исключений и множественный catch. [#219368]

--- src/main/java/ru/job4j/ex/UserInvalidException.java
+++ src/main/java/ru/job4j/ex/UserInvalidException.java
@@ -2,6 +2,6 @@ package ru.job4j.ex;
 
 public class UserInvalidException extends UserNotFoundException {
     public UserInvalidException(String message) {
-        super (message);
+        super(message);
     }
 }
