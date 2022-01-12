e5c4444202dfe056bf984bf239ac8874c7829928
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-01-12 23:19:34 +0500

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String t : text) {
+            check.addAll(origin);
+            if(check.contains(t));
+            return rsl;
+        }
+        return false;
+    }
+}
