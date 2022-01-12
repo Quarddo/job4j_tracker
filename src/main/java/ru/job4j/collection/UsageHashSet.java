024ed4638a95c495d51787cb3d75f420f669f229
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-01-12 17:03:14 +0500

    0. Set, HashSet, Iterator. [#10095 #249935]

--- src/main/java/ru/job4j/collection/UsageHashSet.java
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -4,13 +4,13 @@ import java.util.HashSet;
 
 public class UsageHashSet {
     public static void main(String[] args) {
-        HashSet<String> autos = new HashSet<>();
+        HashSet<String> autos = new HashSet<String>();
         autos.add("Lada");
         autos.add("BMW");
         autos.add("Volvo");
         autos.add("Toyota");
         for (String auto : autos) {
-            System.out.println(autos);
+            System.out.println(auto);
         }
     }
 }
