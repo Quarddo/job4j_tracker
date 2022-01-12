a3e3e161cc86da85d1d85fea4e3cb2dfde53e8c9
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-01-12 16:35:55 +0500

    6. Изменить программу Tracker из 2-го модуля [#10039 #249346]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class Tracker {
     private final List<Item> items = new ArrayList<>();
     private int ids = 1;
-    private int size = 0;
 
     public Item add(Item item) {
         item.setId(ids++);
@@ -35,19 +34,19 @@ public class Tracker {
     private int indexOf(int id) {
         int rsl = -1;
         for (int index = 0; index < items.size(); index++) {
-                if (items.get(index).getId() == id) {
-                    rsl = index;
-                    break;
+            if (items.get(index).getId() == id) {
+                rsl = index;
+                break;
                 }
         }
-            return rsl;
+        return rsl;
     }
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
         if (index != -1) {
-        item.setId(id);
-        items.set(index, item);
+            item.setId(id);
+            items.set(index, item);
             return true;
         }
         return false;
