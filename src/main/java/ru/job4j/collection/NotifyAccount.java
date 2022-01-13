3069f48064e009014373aec9875fd5e264071cd8
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-01-13 16:56:15 +0500

    3. Модели данных и HashSet. [#218530]

---
+++ src/main/java/ru/job4j/collection/NotifyAccount.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.List;
+import java.util.HashSet;
+
+public class NotifyAccount {
+    public static HashSet<Account> sent(List<Account> accounts) {
+        HashSet<Account> rsl = new HashSet<Account>(accounts);
+        for (Account account : accounts) {
+            if (!account.equals(accounts)) {
+                rsl.add(account);
+            }
+        }
+        return rsl;
+    }
+}
