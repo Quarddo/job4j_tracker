2168c90037d51b41c31415ccc052fca11f81f7c0
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-01-13 16:59:53 +0500

    3. Модели данных и HashSet. [#218530 #250366]

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -26,20 +26,19 @@ public class NotifyAccountTest {
         assertThat(NotifyAccount.sent(accounts), is(expect));
     }
 
-
-        @Test
-        public void whenSent() {
-            List<Account> accounts = Arrays.asList(
-                    new Account("123", "Petr Arsentev", "eDer3432f"),
-                    new Account("123", "Petr Arsentev", "eDer3432f"),
-                    new Account("142", "Petr Arsentev", "000001")
-            );
-            HashSet<Account> expect = new HashSet<>(
-                    Arrays.asList(
-                            new Account("123", "Petr Arsentev", "eDer3432f"),
-                            new Account("142", "Petr Arsentev", "000001")
-                    )
-            );
-            assertThat(NotifyAccount.sent(accounts), is(expect));
-        }
+    @Test
+    public void whenSent() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f"),
+                        new Account("142", "Petr Arsentev", "000001")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
 }
