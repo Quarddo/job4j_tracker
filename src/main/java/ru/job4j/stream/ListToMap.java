57179f8723312e05fdc2f5e27f068784e9e6c3cc
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-01-28 20:03:11 +0500

    1.1. Анонимные классы [#214142]

--- src/main/java/ru/job4j/stream/ListToMap.java
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -5,7 +5,20 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * Преобразование списка(List) в карту(Map)
+ */
 public class ListToMap {
+    /**
+     *
+     * @param list проход по списку студентов
+     * Student::getSurname - ключ(фамилия студента)
+     * s -> s - значение (объект ученика)
+     * (s, s1) -> s -  ключ - это фамилия, то дубликатами будут те пары,
+     * у которых совпадают фамилии студентов. В случае добавления
+     * дубликата необходимо оставить старое значение.
+     * @return возвращает карту (с фамилией и значением)
+     */
     public static Map<String, Student> convert(List<Student> list) {
         return list.stream().collect(Collectors.toMap(
                 Student::getSurname,
