2f05ea3f9b790391ab3c6a11b3cb8c8488f0eabe
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-01-30 16:58:42 +0500

    0. Stream API улучшения [#58167]

--- src/main/java/ru/job4j/api/StudentLevel.java
+++ src/main/java/ru/job4j/api/StudentLevel.java
@@ -11,7 +11,7 @@ public class StudentLevel {
         return students.stream()
                 .filter(st -> Objects.nonNull(st))
                 .sorted((left, right) -> Integer.compare(right.getScore(), left.getScore()))
-                .takeWhile(st -> st.getScore() > bound )
+                .takeWhile(st -> st.getScore() > bound)
                 .collect(Collectors.toList());
     }
 }
