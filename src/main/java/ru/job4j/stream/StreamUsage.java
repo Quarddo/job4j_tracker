702c8334620e2adb4691c82511a49c97601fd3ac
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-02-03 12:43:48 +0500

    0. Stream API [#58166 #255876]

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -4,9 +4,19 @@ import java.util.List;
 
 public class StreamUsage {
     public static class Task {
+
+        /**
+         * создаем строковую переменную
+         * создаем переменную long
+         */
         private final String name;
         private final long spent;
 
+        /**
+         * создаем конструктор и инициализируем поля
+         * @param name инициальзация по имени
+         * @param spent инициализация по значению
+         */
         public Task(String name, long spent) {
             this.name = name;
             this.spent = spent;
@@ -19,9 +29,13 @@ public class StreamUsage {
                 new Task("Task #2", 20),
                 new Task("Bug #3", 40)
         );
-        tasks.stream().filter(task -> task.name.contains("Bug"))
-                .filter(task -> task.spent > 30)
-                .map(task -> task.name + ", " + task.spent)
-                .forEach(System.out::println);
+        tasks.stream() /**запускаем поток элементов из созданного листа
+               (если метод возвращает true то элемент передается дальше)*/
+                .filter(task -> task.name.contains("Bug")) /** применяем фильтр где есть "Bug" */
+                .filter(task -> task.spent > 30) /** применяем фильтр где значение меньше 30 */
+                .map(task -> task.name + ", " + task.spent) /** записываем в Map */
+                /** .collect(Collectors.toList()) - прошедшие фильтрацию элементы
+                  * записываются в list */
+                .forEach(System.out::println); /** выводим печать */у
     }
 }
